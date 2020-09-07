dc29f04b7e4f647e63a77e2a30484e745d9badf4
scl <419863966@qq.com>
2020-09-07 17:28:25 +0800

    promise链式调用

--- test3.js
+++ test3.js
@@ -172,3 +172,42 @@ var handler4 = function(value4){
 }
 obj.api1(handler1);
 
+var emitter = new event.Emitter();
+
+emitter.on("step1", function(){
+    obj.api1(function(value1){
+        emitter.emit("step2", value1);
+    });
+});
+
+emitter.on("step2", function(vlue1){
+    obj.api2(value1, function(value2){
+        emitter.emit("step3", vlue2);
+    });
+});
+
+emitter.on("step3", function(value2){
+    obj.api3(value2, function(value3){
+        emitter.emit("step4", value3);
+    });
+});
+emitter.on("step4", function(value3){
+    obj.api4(value3, function(value4){
+        callback(value4);
+    });
+});
+emitter.emit("step1");
+
+promise()
+    .then(obj.api1)
+    .then(obj.api2)
+    .then(obj.api3)
+    .then(obj.api4)
+    .then(function(value4){
+        //do
+    },function(error){
+        //error
+    })
+    .done();
+
+
